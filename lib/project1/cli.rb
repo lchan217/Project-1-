6efe437cf95e0a9f3299496fa85737e5a3ea821f
Lizzie Chan <lchan217@gmail.com>
2019-03-08 23:37:49 +0000

    pry and paths are all corrected

--- lib/project1/cli.rb
+++ lib/project1/cli.rb
@@ -1,24 +1,21 @@
 class CommandLineInterface
   def call 
-    puts "does this still work??"
+    greeting
+    find_choices
+    show_books
   end 
-  # def call 
-  #   greeting
-  #   find_choices
-  #   show_books
-  # end 
-  # def greeting
-  #   puts "Hello! What book would you like more information on?"
-  # end 
-  # def find_choices
-  #   Scraper.find_choices
-  # end 
-  # def show_books 
-  #   puts "Please choose a book."
-  #   Book.all.each_with_index |book, index|
-  #     puts "#{index+1}. #{book.title} by #{book.author} - number of votes: #{votes} = previous award: #{previous_award}"
-  #   end
-  #   input = gets.strip 
-    
-  # end 
+  def greeting
+    puts "Hello! What book would you like more information on?"
+  end 
+  def find_choices
+    Scraper.find_choices
+  end 
+  def show_books 
+    puts "Please choose a book."
+    # Book.all.each_with_index |book, index|
+    #   puts "#{index+1}. #{book.title} by #{book.author} - number of votes: #{votes} = previous award: #{previous_award}"
+    # end
+    # input = gets.strip 
+    binding.pry
+  end 
 end 
