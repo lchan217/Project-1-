f95e0e2484a8173e4180d6944b982d3fa277c872
Lizzie Chan <lchan217@gmail.com>
2019-03-08 22:11:37 +0000

    find_choices, have traits assigned

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,9 @@
 class Book
   attr_accessor :votes, :previous_awards, :title_and_author, :title, :author, :rating, :number_of_ratings
   @@all = [] 
-  def initialize(attributes)
-     attributes.each {|key, value| self.send(("#{key}="), value)}
+  def initialize(title, author)
+    @title = title 
+    @author = author 
      @@all << self 
   end 
   def self.all 
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,22 @@
 require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
 class Scraper 
-  def self.find_choices
-  end 
+  #def self.find_choices
+    html = open('https://www.goodreads.com/choiceawards/best-of-the-best-2018')
+    doc = Nokogiri::HTML(html)
+    
+    array = doc.css('.inlineblock.pollAnswer.resultShown').css('strong.uitext.result').text.split("\n")
+
+    number_of_votes = separated_array = array.each_slice(5).to_a[1][1]
+
+    previous_award = separated_array = array.each_slice(5).to_a[1][4]
+
+    title = doc.css(".pollAnswer__bookLink").css("img").first["alt"].split(" by ")[0]
+
+    author = doc.css(".pollAnswer__bookLink").css("img").first["alt"].split(" by ")[1]
+
+     binding.pry
+  #end 
 end 
