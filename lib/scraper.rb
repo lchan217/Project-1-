8e8a5a4525aa3185ab2358a36f4d8842e4eb5a11
Lizzie Chan <lchan217@gmail.com>
2019-03-08 22:34:11 +0000

    attributes assigned to new_book

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,15 +4,21 @@ require 'open-uri'
 require_relative "book.rb"
 
 class Scraper 
+  URL = 'https://www.goodreads.com/choiceawards/best-of-the-best-2018'
   #def self.find_choices
-    html = open('https://www.goodreads.com/choiceawards/best-of-the-best-2018')
+    html = open(URL)
     doc = Nokogiri::HTML(html)
     
     array = doc.css('.inlineblock.pollAnswer.resultShown').css('strong.uitext.result').text.split("\n")
-    number_of_votes = array.each_slice(5).to_a[1][1]
+    votes = array.each_slice(5).to_a[1][1]
     previous_award = array.each_slice(5).to_a[1][4]
     title = doc.css(".pollAnswer__bookLink").css("img").first["alt"].split(" by ")[0]
     author = doc.css(".pollAnswer__bookLink").css("img").first["alt"].split(" by ")[1]
+    
     new_book = Book.new(title,author)
+    new_book.votes = votes
+    new_book.previous_award = previous_award
+    new_book
+    binding.pry
   #end
 end 
