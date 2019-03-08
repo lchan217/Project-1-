1bb4708d22431cbb1c85487b813a4fe9208a8812
Lizzie Chan <lchan217@gmail.com>
2019-03-08 20:13:43 +0000

    book class set up

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,11 @@
 class Book
+  attr_accessor :votes, :previous_awards, :title_and_author, :title, :author, :rating, :number_of_ratings
+  @@all = [] 
+  def initialize(attributes)
+     attributes.each {|key, value| self.send(("#{key}="), value)}
+     @@all << self 
+  end 
+  def self.all 
+    @@all 
+  end 
 end 
