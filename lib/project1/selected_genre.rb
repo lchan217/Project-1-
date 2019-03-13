d190bf7f5dda358f32341a4ba9a6f3ae95dfa190
Lizzie Chan <lchan217@gmail.com>
2019-03-13 14:32:18 +0000

    title and author attribute added

--- lib/project1/selected_genre.rb
+++ lib/project1/selected_genre.rb
@@ -1,5 +1,5 @@
 class Selected_genre
-   attr_accessor :title_and_author, :total_votes, :url, :description, :rating, :number_of_ratings
+   attr_accessor :title_and_author, :total_votes, :title, :author, :url, :description, :rating, :number_of_ratings
   @@all = [] 
   def initialize(title_and_author, url)
     @title_and_author = title_and_author
